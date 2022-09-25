41b53e00a896dd2c79ee8b6f8bedb022f29bb6f4
vijay <vijayumar.malladi@gmail.com>
2022-09-25 18:53:01 +0530

    updated raun.bat with allure

--- Run.bat
+++ Run.bat
@@ -1,2 +1,2 @@
-pytest -m "regression" --html=./Reports/report.html testCases/
+pytest -m "regression" --html=./Reports/report.html testCases/ --alluredir=./Reports/Allure
 rem pytest -m "regression" --html=./Reports/report.html testCases/ --browser edge
